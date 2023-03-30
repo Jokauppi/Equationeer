8866107a0e10c8dc1dc4368af2da3fd135e6e781
Joona Kauppila <joona.kauppila@helsinki.fi>
2023-03-30 12:21:56 +0300

    create working text mode

--- src/components/KaTeXBox.tsx
+++ src/components/KaTeXBox.tsx
@@ -8,16 +8,30 @@ function KaTeXBox() {
 
   return (
     <Box fit>
-      <div className="flex flex-col justify-start items-center gap-x-2">
+      <div className="flex flex-col h-full justify-start items-center gap-x-2 overflow-hidden">
         <textarea
           placeholder="LaTeX..."
           value={math}
           onChange={(event) => {
             setMath(event.target.value);
           }}
-          className="text-neutral-400 w-full h-fit bg-white dark:bg-neutral-800 focus:outline-none"
+          className="box-border text-neutral-400 w-full bg-white dark:bg-neutral-800 focus:outline-none"
         />
-        <div className="w-fit h-fit py-4 scale-100" dangerouslySetInnerHTML={{ __html: katex.renderToString(math ?? '', { throwOnError: false }) }} />
+        <div
+          className="w-full h-fit py-4"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={
+            {
+              __html: katex.renderToString(String.raw`\text{${math?.replaceAll(/(?<=[a-z]|[,-.]) +(?=[a-z]|[,-.])/gi, '}\\allowbreak\\text{\\space ')}}` ?? '', {
+                throwOnError: false,
+                displayMode: false,
+              }),
+            }
+          }
+        />
+        <div>
+          {String.raw`\text{${math?.replaceAll(/(?<=[a-z]|[,-.]) +(?=[a-z]|[,-.])/gi, '}\\allowbreak\\text{ ')}}`}
+        </div>
       </div>
     </Box>
   );
